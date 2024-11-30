9be64d268e4ae7e62b7f23b9479aa4408f3d006f
Orinameh <memefacto@yahoo.com>
2024-11-30 17:26:05 +0100

    update Readme and remove .env file

--- src/components/Spinner.tsx
+++
@@ -1,8 +0,0 @@
-const Spinner = () => (
-  <div
-    data-testid="spinner"
-    className="w-12 h-12 rounded-full animate-spin mx-auto mt-48 border-2 border-solid border-primary border-t-transparent"
-  />
-);
-
-export default Spinner;
--- src/context/AuthContext.tsx
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-import { AuthType, DispatchType } from "./AuthContextProvider";
-
-type ContextType = { user: AuthType; dispatch: DispatchType };
-
-export const AuthContext = createContext<ContextType>({
-  user: { token: "" },
-  dispatch: () => {},
-});
